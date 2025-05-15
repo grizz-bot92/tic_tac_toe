20773cae6487dc024c3cde85685a33e2569174ea
grizz-bot92 <bbenoit752@gmail.com>
2025-05-14 19:56:16 -0700

    updated logic for winning

--- main.js
+++ main.js
@@ -69,32 +69,22 @@ function playRound(playerOne, playerTwo){
 
 
 function checkWinner(board){
-    if(board[0][1] == board[0][0] && board[0][1] == board[0][2] && board[0][1] != '') {
-        return board[0][1]
-    }
+    for(let i = 0; i < 3; i++){
+        if (board[i][0] !== '' && board[i][0] == board[i][1] && board[i][1] == board[i][2]){
+            return board[i][0]
+        }
+        if (board[0][i] !== '' && board[0][i] == board[1][i] && board[1][i] == board[2][i]){
+            return board[0][i]
+        }
+        if (board[0][0] !== '' && board[0][0] == board[1][1] && board[1][1] == board[2][2]){
+            return board[0][0]
+        }
+        if (board[0][2] !== '' && board[0][2] == board[1][1] && board[1][1] == board[2][0]){
+            return board[0][i]
+        }
 
-    if(board[2][1] == board[2][0] && board[2][2] == board[0][2] && board[2][1] != '') {
-        return board[2][1]
-    }
-
-    if(board[1][0] == board[0][0] && board[1][0] == board[2][0] && board[1][0] != '') {
-        return board[1][0]
-    }
-
-    if(board[1][2] == board[0][2] && board[1][2] == board[0][2] && board[1][2] != '') {
-        return board[1][2]
-    }
-
-    if (
-        board[1][1] != 0 &&
-        ((board[1][1] == board[1][0] && board[1][1] == board[1][2]) ||
-        (board[1][1] == board[0][1] && board[1][1] == board[2][1]) ||
-        (board[1][1] == board[0][0] && board[1][1] == board[2][2]) ||
-        (board[1][1] == board[2][0] && board[1][1] == board[0][2])))
-    {
-        return board[1][1]
-    } 
-    return ' '
+    }    
+            
 }
 
 // function playGame()
